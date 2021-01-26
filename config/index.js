a8aca6ba3a9e2d7dfbaa2aa74b70356323905ef4
stone37170 <stone37170@gmail.com>
2021-01-26 11:20:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   username: process.env.TEXTNOW_USERNAME || "stone37170",
   password: process.env.TEXTNOW_PASSWORD || "19701221shi",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
+  recipient: process.env.TEXTNOW_RECIPIENT || "(805) 817-8741",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
 };
